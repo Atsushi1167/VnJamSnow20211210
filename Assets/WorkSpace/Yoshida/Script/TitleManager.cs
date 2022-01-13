297c1df20b7a3b58b9331b68ef162fef0b818ae5
yoshida1997 <95912587+yoshida1997@users.noreply.github.com>
2022-01-13 09:27:48 +0900

    Yoshida

---
+++ Assets/WorkSpace/Yoshida/Script/TitleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    public Text txtNav;
+    float Elapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Elapsed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Elapsed += Time.deltaTime;
+        Elapsed %= 1.0f;
+        txtNav.text = (Elapsed < 0.8f) ? "Push Any Button" : "";
+    }
+}
